f050e533575b9f96003f1bba8bd956fc2c0fb87b
Nenad TRIPIC <tripicnenad@hotmail.com>
2017-12-20 21:40:55 +0100

    Hausuebung abgegeben

---
+++ lists.pl
@@ -0,0 +1,30 @@
+parent(adam, john).
+parent(eve, john).
+parent(eve, lisa).
+parent(john, anne).
+parent(john, pat).
+parent(pat, jacob).
+parent(carol, jacob).
+
+ancestor(X, Z) :- parent(X, Z).
+ancestor(X, Z) :- parent(X, Y), ancestor(Y, Z).
+
+head(X, [X|T]).
+head(X, [X | _]).
+tail(T, [_ | T]).
+
+head([Head | _], Head).
+tail([_ | Tail], Tail).
+
+isMember(X, [X | _]).
+isMember(X, [_ | Tail]) :- isMember(X, Tail).
+
+concatenate([], L, L).
+concatenate([H|T], L, [H|ExtendedTail]) :- concatenate(T, L, ExtendedTail).
+
+prepend(L, E, [E|L]).
+
+addElement(L, B, [B|L]).
+
+hasLength([],0).
+hasLength([H|T],L+1) :-hasLength(T,L).
